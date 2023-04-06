139934a5dd130e5d30d322a93e5c3292eff4b273
Smartwa <smartwacaleb@gmail.com>
2023-04-06 10:14:31 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -12,11 +12,6 @@ self.addEventListener('message', (event) => {
 
 workbox.core.clientsClaim();
 
-/**
- * The workboxSW.precacheAndRoute() method efficiently caches and responds to
- * requests for URLs in the manifest.
- * See https://goo.gl/S9QRab
- */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
