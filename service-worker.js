8b31b5b9055c37d4d4a3caf0827d2ba202eb5855
Smartwa <smartwacaleb@gmail.com>
2023-04-06 10:13:04 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,16 +1,3 @@
-/**
- * Welcome to your Workbox-powered service worker!
- *
- * You'll need to register this file in your web app and you should
- * disable HTTP caching for this file too.
- * See https://goo.gl/nhQhGp
- *
- * The rest of the code is auto-generated. Please don't update this file
- * directly; instead, make changes to your Workbox build configuration
- * and re-run your build process.
- * See https://goo.gl/2aRDsh
- */
-
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
