8bd4a4bc133ec54bd440cf7337755ab5440ce042
Simatwa <smartwacaleb@gmail.com>
2023-04-07 19:19:01 +0300

    Update favicons

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,20 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.4c4af4f147e464b08d67579b2613bc5a.js"
+  "./precache-manifest.0fd082c039ef1d934a463e062ee6b47b.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -12,6 +25,11 @@ self.addEventListener('message', (event) => {
 
 workbox.core.clientsClaim();
 
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
